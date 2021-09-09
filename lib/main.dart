bd30a32b6d5815a74284020238478e433c4ee812
Roberto Markus <markusrobertoo@gmail.com>
2021-09-09 19:59:43 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './views/pages.dart';
+
+bool? seenOnboard;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // to show status bar
+  SystemChrome.setEnabledSystemUIOverlays(
+      [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+  // to load on board screen for only first and once time
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  seenOnboard = pref.getBool('seenOnboard') ?? false; // if null set to false
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Habit App',
+      theme: ThemeData(
+        textTheme: GoogleFonts.manropeTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      home: seenOnboard == true ? SignUpPage() : OnBoardingPage(),
+    );
+  }
+}
